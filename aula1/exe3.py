b33c96eb69822374fb57607efe8a6fea3b20f03e
SrChrystofer <Cardoso.Chrystofer@gmail.com>
2023-05-04 00:20:33 -0300

    resolvido

--- aula1/exe3.py
+++ aula1/exe3.py
@@ -1,1 +1,5 @@
 # Peça para o usuário inserir a base e altura de um retângulo e imprima sua área
+base= int(input('Informe o valor da base de um retangulo qualquer.'))
+altura= int(input('Me informe o valor da altura deste retangulo.'))
+area= base*altura
+print(f'O valor total da área deste retangulo é: {area} ')
