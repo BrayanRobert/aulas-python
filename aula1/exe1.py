5eb303dd8da3fa1f097a88c7b8f6c039d96a5117
SrChrystofer <Cardoso.Chrystofer@gmail.com>
2023-05-04 00:26:08 -0300

    Resolvido

--- aula1/exe1.py
+++ aula1/exe1.py
@@ -1,3 +1,4 @@
 # Pergunte ao usuário qual o nome dele e imprima uma mensagem de boas vindas
 nome= input('Qual é o seu nome ? ')
-print(f'O nome dele é {nome} ! ')
+print(f'Seja bem vindo {nome} ! ')
+print('Espero que você tenha um bom desenvolvimento durente este curso!')
