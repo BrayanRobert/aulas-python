2f1b8e25047b8961a2ebed2797fd71a3713d315b
SrChrystofer <Cardoso.Chrystofer@gmail.com>
2023-05-04 00:10:32 -0300

    Resolvido

--- aula1/exe2.py
+++ aula1/exe2.py
@@ -1,1 +1,5 @@
 # Pergunte ao usuário seu nome e sua idade e imprima na tela as informações
+nome= input("Qual é o seu nome ?")
+ano= int(input("Em que ano você nasceu ?"))
+idade= 2023 - ano
+print(f'O nome dele é {nome} e tem apenas {idade}! ')
