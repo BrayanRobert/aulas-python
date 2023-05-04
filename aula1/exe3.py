67e8e544c1d62086314cee7187a56f6f497b22b1
GarciaRuas <lipe.garcia@live.com>
2023-05-04 00:33:57 -0300

    resolvendo exercício 3

--- aula1/exe3.py
+++ aula1/exe3.py
@@ -1,1 +1,5 @@
 # Peça para o usuário inserir a base e altura de um retângulo e imprima sua área
+base = int(input("Qual o valor da base do retângulo? "))
+altura = int(input("Qual o Valor da altura do retângulo? "))
+area = base * altura
+print(f"a area do retângulo é: {area}")            
