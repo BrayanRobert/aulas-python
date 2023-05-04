50915254f9f89c535817a5558b14c5fa6f1925a1
Marlon709 <marlon567840@gmail.com>
2023-05-03 23:06:01 -0300

    Resolvendo o exercicio 2

--- aula1/exe2.py
+++ aula1/exe2.py
@@ -1,1 +1,5 @@
 # Pergunte ao usuário seu nome e sua idade e imprima na tela as informações
+nome = input('Qual o seu nome?')
+idade = input('Qual a sua idade?')
+
+print(f"Seu nome é:{nome} e sua idade é:{idade}")
