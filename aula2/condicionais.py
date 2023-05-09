b57fad3b558b17b4e4c65ad35c92bb25b343575a
magnoDuraes <magnodbf962@gmail.com>
2023-05-08 22:35:45 -0300

    adicionando toda a explicação das condicionais

--- aula2/condicionais.py
+++ aula2/condicionais.py
@@ -8,3 +8,15 @@ if idade < 18:
   print("Você é menor de idade")
 else:
   print("Você é maior de idade")
+
+#--------------------------------- 
+sinal = 'verde'
+
+if sinal == 'verde':
+  print("Atravessando")
+elif sinal == 'amarelo':
+  print("Atravessando correndo")
+elif sinal == 'vermelho':
+  print("Atravessando correndo")
+else:
+  print("Que sinal é esse que eu não conheço?")
