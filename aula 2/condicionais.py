768ee656365cb96e90b8fbe8fc31f6ee27c8a502
SrChrystofer <Cardoso.Chrystofer@gmail.com>
2023-05-11 01:21:48 -0300

    Aula 2

---
+++ aula 2/condicionais.py
@@ -0,0 +1,14 @@
+# Estruturas condicionais em python
+#if,else, elif
+#if é usado
+
+sinal = 'azul'
+
+if sinal == 'verde':
+    print('atravessando')
+elif sinal == 'amarelo':
+    print('atravessando')
+elif sinal == 'vermelho':
+    print('Estou parado')
+else:
+    print('Que sinal é esse que não conheço')
---
+++ aula 2/ex1.py
@@ -0,0 +1,18 @@
+# Faça um programa que pegue o nome do aluno, receba duas notas que ele teve em uma prova, e faça a média dessas notas, se a média for
+# maior ou igual a 7, mostre que ele foi aprovado, se a média, ficou maior ou igual a 3, porém menor que 7, mostre que ele foi pra rec
+# se a nota for maior que 7, mostre que ele foi aprovado, se a média, ficou maior
+nome= input('Olá, qual é o seu nome ? ')
+nota1= float(input(f'{nome}, me informe a nota da primeira prova do trimestre: '))
+nota2= float(input('Agora a nota da segunda prova: '))
+media= float((nota1+nota2)/2)
+
+if media >=7:
+       print(f'{nome}, sua média é {media}. Parabéns, você foi aprovado!!!! ')
+elif media >= 3:
+       print(f'{nome}, sua média é {media}. Você está de recuperação.')
+elif media <3:
+       print(f'{nome}, sua média é {media}. Sinto muito, mas você está reprovado. ')       
+  
+
+
+
