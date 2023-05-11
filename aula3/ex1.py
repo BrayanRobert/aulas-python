0c529b889be7c07762635e65f69125483ff7c0e5
Marlon709 <marlon567840@gmail.com>
2023-05-10 23:50:44 -0300

    Exercicio 1 da aula 4

---
+++ aula3/ex1.py
@@ -0,0 +1,15 @@
+# Faça um programa que dê ao usuário 4 opções:
+# 1 - Somar 2 + 2. 2 - Multiplicar 3 vezes 5, 3 - Mostra que é rio de janeiro, 4 - Sair do programa
+
+while True:
+    contador = int(input('Digite uma opção'))
+    if contador == 1:
+        print(2+2)
+    elif contador == 2:
+        print(3*5)
+    elif contador == 3:
+        print('Estamos no Rio De Janeiro')
+    elif contador == 4:
+        break
+    else:
+        print('Voce digitou uma opção invalidada seu BURRO!!!')
