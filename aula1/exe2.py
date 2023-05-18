c4fd67d13325ace54303201f2f5dc465d43f36d4
Shinrou10 <macedo.la387@gmail.com>
2023-05-17 23:56:34 -0300

    exemplo2

--- aula1/exe2.py
+++ aula1/exe2.py
@@ -1,1 +1,11 @@
 # Pergunte ao usuário seu nome e sua idade e imprima na tela as informações
+
+idade = input("Qual sua idade? ")
+
+nome = input("Qual seu nome? ")
+
+String1 = (" não sabia que você tinha ")
+String2 =  nome
+String3 = idade
+String4 = String2 + String1 + String3
+print(String4)
