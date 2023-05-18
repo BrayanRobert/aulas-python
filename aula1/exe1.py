22c82193c14d2f6e6f10f2960d224ed7f8fbd96b
Shinrou10 <macedo.la387@gmail.com>
2023-05-17 23:33:17 -0300

    exemplo1

--- aula1/exe1.py
+++ aula1/exe1.py
@@ -1,1 +1,7 @@
 # Pergunte ao usuário qual o nome dele e imprima uma mensagem de boas vindas
+nome = input("Qual seu nome? ") 
+
+String1 = nome
+String2 = 'Olá !'
+String3 = String2 + String1
+print(String3)
