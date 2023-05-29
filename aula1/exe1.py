cfc026b067a7159a33fc3ad5d622c51fd131788d
Mastrupio <Leopandylm@gmail.com>
2023-05-29 19:08:25 -0300

    fazendo exercícios 1 e 2

--- aula1/exe1.py
+++ aula1/exe1.py
@@ -1,3 +1,4 @@
 # Pergunte ao usuário qual o nome dele e imprima uma mensagem de boas vindas
 nome = input("Qual é o seu nome? ")
-print(f"Seja bem vindo {nome}")
+print("Seja bem vindo", nome)
+
--- aula1/exe2.py
+++ aula1/exe2.py
@@ -1,1 +1,5 @@
 # Pergunte ao usuário seu nome e sua idade e imprima na tela as informações
+print("Olá, por favor me forneça as seguintes informações:")
+nome = input("Seu nome")
+idade = input("Sua idade")
+print("Seu nome é", nome, "e vc possui", idade, "anos")
