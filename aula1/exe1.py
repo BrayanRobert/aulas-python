36a73a53a90da90032855a2545e3b37ef3208271
Mastrupio <Leopandylm@gmail.com>
2023-05-29 19:34:20 -0300

    fazendo exercício 3

--- aula1/exe1.py
+++ aula1/exe1.py
@@ -1,4 +1,3 @@
 # Pergunte ao usuário qual o nome dele e imprima uma mensagem de boas vindas
 nome = input("Qual é o seu nome? ")
 print("Seja bem vindo", nome)
-
--- aula1/exe3.py
+++ aula1/exe3.py
@@ -1,1 +1,6 @@
 # Peça para o usuário inserir a base e altura de um retângulo e imprima sua área
+print("Para que a área de um retângulo possa ser calculada, defina os seguintes valores utilizando um número inteiro:")
+base = int(input("base:"))
+altura = int(input("altura"))
+área = base * altura
+print("A área desse retângulo é de", área, "m²")
