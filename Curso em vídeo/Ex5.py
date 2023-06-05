fe8d45e54d056df4b7d24b16af23ff8b940e40e5
Mastrupio <Leopandylm@gmail.com>
2023-06-05 15:15:23 -0300

    resolução exercício 4, 5 e 6 curso em vídeo Python

---
+++ Curso em vídeo/Ex5.py
@@ -0,0 +1,2 @@
+x = int(input("Digita um valor"))
+print("{}, {}, {}".format(x-1, x, x+1))
---
+++ Curso em vídeo/Ex6.py
@@ -0,0 +1,2 @@
+x = float(input("Digite um valor"))
+print("O dobro de {} é: {} \nO triplo de {} é: {} \nA raiz quadrada de {} é: {}".format(x, x*2, x, x*3, x, x**(1/2)))
