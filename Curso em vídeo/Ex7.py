0d3de6dd414a7f2435a26b63aacc5ee77610ce84
Mastrupio <Leopandylm@gmail.com>
2023-06-05 15:48:49 -0300

    exercício 1 aula 3

---
+++ Curso em vídeo/Ex7.py
@@ -0,0 +1,9 @@
+x = float(input("Resultado da prova"))
+y = float(input("Resultado do teste"))
+z = (x+y)/2
+if z >=7:
+    print("Aprovado")
+elif z >=3:
+    print("Recuperação")
+else:
+    print("Reprovado")
--- aula3/ex1.py
+++ aula3/ex1.py
@@ -1,2 +1,9 @@
 # Faça um programa que dê ao usuário 4 opções:
 # 1 - Somar 2 + 2. 2 - Multiplicar 3 vezes 5, 3 - Mostra que é rio de janeiro, 4 - Sair do programa
+valor = int(input("choose a number from 1 to 4 "))
+if valor ==1:
+    print(2+2)
+elif valor ==2:
+    print(3*5)
+elif valor ==3:
+    print("é rio de janeiro")
